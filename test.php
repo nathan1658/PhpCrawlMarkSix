fdb692276cb9608e51cad62442492397a6da8289
nathan1658 <pooikei165@gmail.com>
2017-12-29 22:37:30 +0800

    Add files via upload
    initial commit

---
+++ test.php
@@ -0,0 +1,31 @@
+<?
+
+define("MARKSIXURL","http://bet.hkjc.com/marksix/index.aspx?lang=ch");
+
+// 建立CURL連線
+$ch = curl_init();
+
+// 設定擷取的URL網址
+curl_setopt($ch, CURLOPT_URL, constant("MARKSIXURL"));
+curl_setopt($ch, CURLOPT_HEADER, false);
+//將curl_exec()獲取的訊息以文件流的形式返回，而不是直接輸出。
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+// 執行
+$temp=curl_exec($ch);
+curl_close($ch);
+
+//echo $temp;
+$dom = new DOMDocument();
+//從一個字符串加載HTML
+@$dom->loadHTML($temp);
+$xpath = new DOMXPath($dom);
+$xpathResult = $xpath->evaluate("//*[@id='oddsTable']/table/tr[3]/td/table/tr[2]/td/table/tr/td[1]/table/tr[2]/td/table/tr");
+$result = [];
+result(1)="sad";
+foreach ($xpathResult as $node) {
+    $result[] = $dom->saveHTML($node);
+}
+print_r($result);
+
+?>
